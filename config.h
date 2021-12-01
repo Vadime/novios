c6486c218e172fbb0c499c6a6ea7a241a9120b24
Vadime <novadim0@gmail.com>
2021-12-01 11:58:10 +0100

    Shity version

---
+++ config.h
@@ -0,0 +1,5 @@
+static const unsigned int cornerrad = 8;
+static const char *fonts[] = {"MonoLisa:pixelsize=14:antialias=true:autohint=true"};
+static const char col_background[] = "#282c34";
+static const char col_foreground[] = "#abb2bf";
+static const char col_selected_f[] = "#61afef";
